96249c8bc9c33506fd76811b17db4e1505b3746a
Godefray <aslyskov@mail.ru>
2022-05-22 20:38:02 +0300

    init commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
-const {src, dest} = require('gulp')
-const sass = require('gulp-sass')
+const {src, dest, series, watch} = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
 const csso = require('gulp-csso')
 const include = require('gulp-file-include')
 const htmlmin = require('gulp-htmlmin')
 const del = require('del')
 const sync = require('browser-sync').create()
+const concat = require('gulp-concat')
+const autoprefixer = require('gulp-autoprefixer')
 
-function html() {
+
+
+async function html() {
     src('src/**.html')
         .pipe(include({
             prefix: '@@'
@@ -14,4 +18,32 @@ function html() {
         .pipe(dest('dist'))
 }
 
-exports.html = html
+
+
+function scss() {
+    return src('src/scss/**.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(csso())
+        .pipe(concat('index.css'))
+        .pipe(dest('dist'))
+}
+
+function clear() {
+    return del('dist')
+}
+
+async function serve() {
+    sync.init({
+        server: './dist'
+    })
+
+    watch('scr/**.html', series(html)).on('change', sync.reload)
+    watch('scr/scss/**.scss', series(scss)).on('change', sync.reload)
+}
+
+exports.build = series(clear, scss, html)
+exports.clear = clear
+exports.serve = series(clear, scss, html, serve)
